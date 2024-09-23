5859881d4e5ecd30ecd04b7eafbabe631181f64c
patriolivadev <patriolivadev@users.noreply.github.com>
2024-09-23 20:53:44 +0000

    Ready to clone and code.

---
+++ android/app/src/main/kotlin/patricio_oliva_test_task_solid_software/patricio_oliva_test_task_solid_software/MainActivity.kt
@@ -0,0 +1,5 @@
+package patricio_oliva_test_task_solid_software.patricio_oliva_test_task_solid_software
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
